ccbf38dbab5fc5209cccecf499801bc3430887a1
ghaerdi <ghaerdi@outlook.com>
2021-10-09 19:58:53 -0400

    formatting

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ mod sort;
 use rand::seq::SliceRandom;
 
 fn main() {
-	let mut arr = (0..5).collect::<Vec<i32>>();
-	arr.shuffle(&mut rand::thread_rng());
-	sort::merge(&mut arr);
+  let mut arr = (0..5).collect::<Vec<i32>>();
+  arr.shuffle(&mut rand::thread_rng());
+  sort::merge(&mut arr);
 
-	println!("array after merge sorted: {:?}", arr);
+  println!("array after merge sorted: {:?}", arr);
 }
