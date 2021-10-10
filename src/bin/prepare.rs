81959d1344dc08c6f346aec0400d9659bf1662e1
ghaerdi <ghaerdi@outlook.com>
2021-10-09 22:09:32 -0400

    formatting

--- src/bin/prepare.rs
+++ src/bin/prepare.rs
@@ -1,8 +1,8 @@
 use std::process::Command;
 
 fn main() {
-	Command::new("cp")
-		.args(["-a", ".githooks/.", ".git/hooks/"])
-		.output()
-		.expect("failed to execute process");
+  Command::new("cp")
+    .args(["-a", ".githooks/.", ".git/hooks/"])
+    .output()
+    .expect("failed to execute process");
 }
