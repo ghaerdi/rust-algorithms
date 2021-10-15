d8686d9aebc89987d6a47fb26a4caf79868debc4
ghaerdi <ghaerdi@outlook.com>
2021-10-15 09:23:37 -0400

    format

--- src/collections/linked_list.rs
+++ src/collections/linked_list.rs
@@ -1,43 +1,40 @@
 #[derive(Debug)]
 struct Node<T> {
-	value: T,
-	next: Option<Box<Node<T>>>,
+  value: T,
+  next: Option<Box<Node<T>>>,
 }
 
 impl<T> Node<T> {
-	fn new(value: T) -> Self {
-		Node {
-			value: value,
-			next: None,
-		}
-	}
+  fn new(value: T) -> Self {
+    Node {
+      value: value,
+      next: None,
+    }
+  }
 }
 
 #[derive(Debug)]
 pub struct LinkedList<T> {
-	head: Option<Box<Node<T>>>,
-	len: usize
+  head: Option<Box<Node<T>>>,
+  len: usize,
 }
 
 impl<T> LinkedList<T> {
-	pub fn new() -> Self {
-		return LinkedList {
-			head: None,
-			len: 0,
-		}
-	}
+  pub fn new() -> Self {
+    return LinkedList { head: None, len: 0 };
+  }
 
-	pub fn push(&mut self, value: T) {
-		let mut new_node = Box::new(Node::new(value));
-		match self.head.take() {
-			Some(old_head) => {
-				new_node.next = Some(old_head);
-				self.head = Some(new_node);
-			},
-			None => {
-				self.head = Some(new_node);
-			}
-		}
-		self.len += 1;
-	}
+  pub fn push(&mut self, value: T) {
+    let mut new_node = Box::new(Node::new(value));
+    match self.head.take() {
+      Some(old_head) => {
+        new_node.next = Some(old_head);
+        self.head = Some(new_node);
+      }
+      None => {
+        self.head = Some(new_node);
+      }
+    }
+    self.len += 1;
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,29 @@ mod sort;
 // use std::collections::LinkedList;
 mod collections;
 
-
-#[derive(Debug)]
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 struct Person {
-    name: String,
-    age: u8,
-    gender: String
+  name: String,
+  age: u8,
+  gender: String,
 }
 
 impl Person {
-    fn new(name: &str, age: u8, gender: &str) -> Self {
-        return Person{
-            name: name.to_string(),
-            gender: gender.to_string(),
-            age
-        }
-    }
-    fn birthday(&mut self) {
-        self.age += 1;
-    }
+  fn new(name: &str, age: u8, gender: &str) -> Self {
+    return Person {
+      name: name.to_string(),
+      gender: gender.to_string(),
+      age,
+    };
+  }
+  fn birthday(&mut self) {
+    self.age += 1;
+  }
 }
 
 fn main() {
-    let mut numbers = collections::LinkedList::<u32>::new();
-    numbers.push(10);
-    numbers.push(11);
-    println!("{:#?}", numbers);
+  let mut numbers = collections::LinkedList::<u32>::new();
+  numbers.push(10);
+  numbers.push(11);
+  println!("{:#?}", numbers);
 }
